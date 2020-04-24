29f33c257005087244232b8fdbbf2863a3619103
patricia <psamper2@illinois.edu>
2020-04-24 09:26:40 -0500

    .

--- sketches/Test_Sync_and_FairSplitting/ARD_FairSplitting/FairSplit_ARD1/FairSplit_ARD1.ino
+++ sketches/Test_Sync_and_FairSplitting/ARD_FairSplitting/FairSplit_ARD1/FairSplit_ARD1.ino
@@ -1,3 +1,4 @@
+
 #include <Streaming.h>
 #include <XBee.h>
 #include <OGraph.h>
@@ -6,18 +7,19 @@
 
 long base = 1e4;  // not using floating points so need a base number
 float tCmd;
-float gi;
+float bi;
 uint8_t i=1;//number of inneighbors
 
 //Create objects needed for communication and control
 XBee xbee = XBee();
 ZBRxResponse rx = ZBRxResponse();
 // address, min, max, alpha, beta, out-degree, base
-OLocalVertex s = OLocalVertex(0x415786E1,9,0,0,0,0,i,base);    //sets up parameters for local vertex
-// OLocalVertex s = OLocalVertex(0x415786E1,0,0.225*D_base,2.1667*base,0.1667*base,5,D_base,9);
+OLocalVertex s = OLocalVertex(0x415786E1,9,3,1,0,0,i,base);    //sets up parameters for local vertex
+// OLocalVertex s = OLocalVertex(0x415786E1,0,0.225*D_base
+//OLocalVertex(XBeeAddress64 a, uint8_t nodeID, float min, float max, float alpha, float beta, uint8_t Dout, long base);2.1667*base,0.1667*base,5,D_base,9);
 LinkedList l = LinkedList();
-OGraph g = OGraph(&s,&l);//pasamos por refencia los objetos de las clases OLocalVertex y LinkedList para crear un objeto de la classe OGragh 
-OAgent a = OAgent(&xbee,&rx,&g,true,true);//(xbee,rx,leader,...) 
+OGraph g = OGraph(&s,&l);
+OAgent a = OAgent(&xbee,&rx,&g,true,true);//(xbee,rx,leader,...)
 
 uint8_t errorPin = 6;  // error led pin
 uint8_t sPin = 7;      // synced led
@@ -30,31 +32,32 @@ void setup()
 {
   Serial.println("start sync");
   Serial.begin(38400);
-  Serial.begin(38400);
+  Serial3.begin(38400);
   pinMode(13, OUTPUT);
   pinMode(sPin, OUTPUT);
   pinMode(errorPin,OUTPUT);
   pinMode(49,OUTPUT);
-  xbee.setSerial(Serial);
+  xbee.setSerial(Serial3);
 
-  g.addInNeighbor(0x415786D3,10,0,0);   // node 10
+  g.addInNeighbor(0x415786D3,10,0,0,3,0);   // node 10
+  //bool addInNeighbor(XBeeAddress64 a, uint8_t neighborID, float r, float x,float fpmax, float fpmin);
 //  g.addNonNeighbor(0x415DB664);  // node 14
 g.configureLinkedList();
 }
 void loop()
 {
-  if(de == false)// en el caso de que no se halla sincronizado 
+  if(de == false)
   {
-    if(!(a.isLeader())) //si no es leader 
+    if(!(a.isLeader()))
     {
       Serial.println("Still trying to sync");
-      if(a.sync()) // si sincroniza 
+      if(a.sync()) 
       {
         Serial.println("Communication Link established");
         Serial.println("c");
         delay(10);
         digitalWrite(sPin,HIGH);
-        de = true; // cambiamos el valor de la variable para indicar que se ha establecido sincronización
+        de = true;
       }
       else
       {
@@ -103,16 +106,19 @@ void loop()
     {
      //run fair splitting algorithm
       tCmd = a.fairSplitRatioConsensus_RSL(50, 1, 30, 200);
-     gi = a.feasibleFlowAlgorithm_RSL(50, 200,2);
-      //feasibleFlowAlgorithm( uint8_t iterations, uint16_t period,float gmax,float gmin, float li, float fijmax[],float fijmin[])
-     
-      //Serial.print(‘’The value of the generation is : “);
-   
-       Serial.print("The value of the RC algorithm is: ");
-     // Serial.println(tCmd);
-     
-      Serial.print("The value of the bi algorithm is: ");
-      Serial.println(gi);
+
+      //feasibleFlowAlgorithm_RSL( uint8_t iterations, uint16_t period)
+      bi = a.feasibleFlowAlgorithm_RSL(30, 200);
+      
+
+      Serial.print();
+      Serial.print("The value of the RC algorithm is: ");
+      Serial.println(tCmd);
+
+      Serial.print();
+      Serial.print("The value of the feasible flow algorithm is: ");
+      Serial.println(bi);
+
       int bbbb = Serial.read();
 
       while(Serial.available()==0)
@@ -122,4 +128,13 @@ void loop()
        a.resync();
     }
   }
+}
+
+}
+
+void loop() {
+  Serial.print("Leader time is ");
+  unsigned long LeaderT = millis();
+  Serial.println(LeaderT);
+  delay(3000);
 }
